c5cfab3f6114995e14eac4677c02c8905cd8320a
liukluke <luca.manfredi1987@gmail.com>
2019-08-26 13:11:13 +0200

    fix eslint

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    <router-link to="/"><img src="https://www.onesaitplatform.online/web/pokemon-exercise/img/pokeball.eefa14c7.svg" alt="Back to home" /></router-link>
     <nav>
       <router-link to="/">Home</router-link>|
       <router-link to="/pokemons/search">Search</router-link>
